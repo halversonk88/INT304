ac1179f3b1be757f93e4b9a028f63114523a9fc1
halversonk88 <halversonk@comcast.net>
2026-08-09 13:20:23 -0700

    Add files via upload
    Updates for week 3 assignment.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import React, {useState} from 'react';
 import './App.css';
 import EmployeeForm from './components/EmployeeForm';
 
 function App() {
+
+  // initialize variable state, start with blank array
+  const [employees, setEmployees] = useState(() => {
+    const storedData = localStorage.getItem('employees');
+    return storedData ? JSON.parse(storedData) : [];
+  });
+
+  // maintain an array of current employees
+  const addEmployee = (employee) => {
+    setEmployees([...employees, employee]);  
+    //setEmployees will override existing state. use ...employees to maintain the array, plus the variable employee
+  };
+
+  // save the employee array to local storage
+  const saveData = () => {
+    localStorage.setItem('employees', JSON.stringify(employees));
+  };
+
   return (
-    <div className="container">
-      <EmployeeForm />
+    <div className="App">
+      <EmployeeForm onAddEmployee={addEmployee} /> 
+      <br />
+      <button onClick={saveData}>Save Data</button>
+       <br />
+      <h2>Current Employees</h2>
+        {employees.map((emp,index)=>
+          <p key={index}>
+              <b>Name: </b> {emp.name}, 
+              <b>Email: </b> {emp.email},
+              <b>Title: </b> {emp.title}, 
+              <b>Department: </b> {emp.department}
+          </p>
+            )}
+       
     </div>
   );
 }
