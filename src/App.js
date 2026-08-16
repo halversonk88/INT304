1ac3925a6546cb801b138603330c3cd462baf355
halversonk88 <halversonk@comcast.net>
2026-08-16 13:39:31 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './CSS/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import EmployeeForm from './components/EmployeeForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeDetail from './components/EmployeeDetail';
 
 function App() {
 
-  // initialize variable state, start with blank array
-  const [employees, setEmployees] = useState([]);
+  // initialize variable state: load from localstorage if available, blank if not
+    const [employees, setEmployees] = useState(() => {
+    const savedEmployees = localStorage.getItem('employees');
+    return savedEmployees ? JSON.parse(savedEmployees) : [];
+  });
+
 
   // maintain an array of current employees
-  const addEmployee = (employee) => {
-    setEmployees([...employees, employee]);
+  const addEmployee = (newEmployee) => {
+    setEmployees((employees) => [...employees, newEmployee]);
   };
 
   // save the employee array to local storage
@@ -23,6 +28,7 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <Routes>
+    // Option 1: Home page shows form and list
         <Route path="/" element={
           <>
             <EmployeeForm onAddEmployee={addEmployee} />
@@ -30,7 +36,13 @@ function App() {
             <button onClick={saveData}>Save Data</button>
           </>
         } />
-        <Route path="/employees/:id" element={<EmployeeDetail employees={employees} />} />
+    // Option 2: Emp list and employee detail
+        <Route path="/employees/:name" element={
+          <>
+            <EmployeeList employees={employees} />
+            <EmployeeDetail employees={employees} />
+          </>
+        } />
       </Routes>
     </div>
   </BrowserRouter>
